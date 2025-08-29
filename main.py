e5cb818d3fd0cec4ddd760bdaa411fcd7697fd40
MatrixShadeYT <mpro9234@gmail.com>
2025-08-29 17:03:15 -0600

    Images

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 from selenium import webdriver
 from time import sleep
 from io import BytesIO
 from PIL import Image
 
 options = Options()
-options.add_argument("--headless")
+#options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=options)
 driver.get("https://chromedino.com/")
-sleep(3)
+def movements(key=0):
+    if key == 1:
+        ActionChains(driver).send_keys(Keys.ARROW_UP).perform()
+    elif key == 2:
+        ActionChains(driver).send_keys(Keys.ARROW_DOWN).perform()
+    else:
+        ActionChains(driver).send_keys(Keys.SPACE).perform()
+    return
+sleep(1)
+movements()
+sleep(1)
 bytes = driver.get_screenshot_as_png()
-driver.save_screenshot("dino.png")
-print(f"\nScreenshot captured ({len(bytes)} bytes)")
 image = Image.open(BytesIO(bytes))
-pixels = list(image.getdata())
-print(f"Image Size: {image.size}")
-print(f"Pixels: {pixels[:1]}")
+for x in range(100):
+    for y in range(25):
+        image.putpixel([1100+x,170+y],value=(0,255,0))
+for x in range(750):
+    for y in range(200):
+        image.putpixel([450+x,200+y],value=(0,0,255))
+image.show()
 driver.quit()
