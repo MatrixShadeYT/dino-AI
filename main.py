1d7755ea716f09f5826e46a2466d4fd0f866defe
MatrixShadeYT <mpro9234@gmail.com>
2025-08-29 14:10:04 -0600

    Basic website screenshot

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+driver = webdriver.Chrome(options=options)
+driver.get("chrome://dino")
+
+def screenshot(name="dino.png"):
+    driver.save_screenshot(name)
+    return name
+
+driver.quit()
