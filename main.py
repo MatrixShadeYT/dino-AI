1c6f6f55c1cd59a8d042f3e9512bc253d2aae2fb
MatrixShadeYT <mpro9234@gmail.com>
2025-08-29 14:34:52 -0600

    Screenshot

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from time import sleep
 
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=options)
-driver.get("chrome://dino")
+driver.get("https://chromedino.com/")
+driver.save_screenshot("screenshot.png")
+screenshot_bytes = driver.get_screenshot_as_png()
 
-def screenshot(name="dino.png"):
-    driver.save_screenshot(name)
-    return name
-
+print(f"\nScreenshot captured ({len(screenshot_bytes)} bytes)")
+print("Program Over!")
 driver.quit()
