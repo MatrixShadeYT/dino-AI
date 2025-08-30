ae46614113384171806dcb369b9a8592b52ae62e
MatrixShadeYT <mpro9234@gmail.com>
2025-08-29 20:52:10 -0600

    Score reading

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from time import sleep
 from io import BytesIO
 from PIL import Image
+import pytesseract
 
 options = Options()
 #options.add_argument("--headless")
@@ -24,9 +25,11 @@ def screenshot():
     bytes = driver.get_screenshot_as_png()
     image = Image.open(BytesIO(bytes))
     high_score = image.crop((1000,170,1100,195))
+    high_score = int(pytesseract.image_to_string(high_score))
     curr_score = image.crop((1100,170,1200,195))
+    curr_score = int(pytesseract.image_to_string(curr_score))
     map = image.crop((450,200,1200,400))
-    return [image,high_score,curr_score,map]
+    return [image,map,high_score,curr_score]
 
 sleep(2)
 movements()
@@ -35,3 +38,5 @@ while True:
     break
 img = screenshot()
 driver.quit()
+print(f"\nCurrent: {img[2]}\nHigh: {img[1]}\n")
+img[0].show()
