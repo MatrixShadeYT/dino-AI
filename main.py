58dedcccf774b7bf4a66058094fc30b541b3ff9b
MatrixShadeYT <mpro9234@gmail.com>
2025-08-29 21:56:35 -0600

    get values

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from io import BytesIO
 from PIL import Image
 import pytesseract
 
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 options = Options()
 #options.add_argument("--headless")
 options.add_argument("--disable-gpu")
@@ -24,19 +25,28 @@ def movements(key=0):
 def screenshot():
     bytes = driver.get_screenshot_as_png()
     image = Image.open(BytesIO(bytes))
-    high_score = image.crop((1000,170,1100,195))
-    high_score = int(pytesseract.image_to_string(high_score))
-    curr_score = image.crop((1100,170,1200,195))
-    curr_score = int(pytesseract.image_to_string(curr_score))
-    map = image.crop((450,200,1200,400))
-    return [image,map,high_score,curr_score]
+    high_score = pytesseract.image_to_string(image.crop((1010,170,1100,195))).upper().replace("O","0").replace("L","")
+    curr_score = pytesseract.image_to_string(image.crop((1110,170,1200,195))).upper().replace("O","0").replace("L","")
+    map = image.crop((450,200,850,400))
+    return [image,map,int(high_score),int(curr_score)]
 
-sleep(2)
+sleep(1.5)
 movements()
+sleep(6)
+movements()
+
 while True:
-    sleep(6)
+    sleep(0.01)
+    img = screenshot()
+    listed = [img[2],img[3]]
+    pixels = list(img[1].convert("RGB").getdata())
+    for i in range(pixels):
+        for x in range(pixels[i+1]):
+            listed.append(pixels[i+1])
+    AI(img)
     break
+
+sleep(1)
 img = screenshot()
 driver.quit()
-print(f"\nCurrent: {img[2]}\nHigh: {img[1]}\n")
-img[0].show()
+print(f"\nCurrent: {img[3]}\nHigh: {img[2]}\n")
