f0167b5423fb58c323e6b615b67045b7d8fb0990
MatrixShadeYT <mpro9234@gmail.com>
2025-08-30 12:25:59 -0600

    12:25 XD

--- images.py
+++ images.py
@@ -2,14 +2,10 @@ from io import BytesIO
 from PIL import Image
 import pytesseract
 
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 def clean_text(text):
     return text.upper().replace("O","0").replace("I","1").replace("L","").replace("S","5").replace("Z","2")
-
-def movements(key=0):
-    if key == 1:
-        ActionChains(driver).send_keys(Keys.ARROW_UP).perform()
-    elif key == 2:
-        ActionChains(driver).send_keys(Keys.ARROW_DOWN).perform()
 
 def screenshot(driver):
     bytes = driver.get_screenshot_as_png()
--- main.py
+++ main.py
@@ -3,28 +3,32 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 from selenium import webdriver
 from time import sleep
+import numpy as np
 import images
 import AI
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 options = Options()
 options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=options)
 driver.get("https://chromedino.com/")
 Neuron = AI.NeuralNetwork([48001,3])
 
+def movements(key=0):
+    if key == 1:
+        ActionChains(driver).send_keys(Keys.ARROW_UP).perform()
+    elif key == 2:
+        ActionChains(driver).send_keys(Keys.ARROW_DOWN).perform()
+
 sleep(2)
 movements(1)
 sleep(6)
 movements(1)
 
-while True:
+for i in range(1):
     sleep(0.001)
-    img = images.screenshot(drive)
-    listed = list(img[1].getdata())
-    extra = [listed[i][0] for i in range(len(listed))]
-    extra.insert(0,img[3])
-    print([i for i in Neuron.forward(extra,method="softmax")])
+    img = images.screenshot(driver)
+    extra = [list(img[1].getdata())[i][0] for i in range(len(list(img[1].getdata())))]
+    print(np.argmax(Neuron.forward(extra,method='softmax'),axis=0))
     break
 
 sleep(1)
