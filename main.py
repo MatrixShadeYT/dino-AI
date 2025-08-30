1b62a6555bb282413f9d357af03a01fcc067f634
MatrixShadeYT <mpro9234@gmail.com>
2025-08-30 00:12:44 -0600

    Check for data

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ options = Options()
 options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=options)
 driver.get("https://chromedino.com/")
-data = save.get()
+data = save.get() if os.path.exists("data.json") else ""
 network = AI.create_network(240001,[2,8],3) if data == "" else data
 new_network = AI.mutate_network(network)
 frames = 1
